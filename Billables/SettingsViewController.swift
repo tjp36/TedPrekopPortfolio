e20e98ba0f4e063e28fbf7b89db1694e608b9579
Theodore Prekop <tjp36@uchicago.edu>
2016-08-22 18:18:14 -0500

    autolayout fixed

--- Billables/SettingsViewController.swift
+++ Billables/SettingsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
     // MARK: Properties
     @IBOutlet weak var firstName: UITextField!
     @IBOutlet weak var lastName: UITextField!
@@ -20,7 +20,7 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if let savedSettings = loadSettings(){
             firstName.text = savedSettings.firstName
             lastName.text = savedSettings.lastName
@@ -29,22 +29,21 @@ class SettingsViewController: UIViewController {
             billableRate.text = String(savedSettings.rate)
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     // MARK: Actions
     @IBAction func saveSettings(sender: AnyObject) {
@@ -71,7 +70,7 @@ class SettingsViewController: UIViewController {
     func loadSettings() -> User? {
         return NSKeyedUnarchiver.unarchiveObjectWithFile(User.ArchiveURL.path!) as? User
     }
-
     
-
+    
+    
 }
--- Billables/User.swift
+++ Billables/User.swift
@@ -5,7 +5,6 @@
 //  Created by Theodore Prekop on 8/14/16.
 //  Copyright © 2016 Theodore Prekop. All rights reserved.
 //
-
 import Foundation
 
 class User{
