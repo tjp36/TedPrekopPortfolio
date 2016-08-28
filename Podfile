c16bfaf74923ea040df5b86edee1a84524c043b0
Theodore Prekop <tjp36@uchicago.edu>
2016-08-28 15:41:28 -0400

    add marketing materials

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, "9.0"
+use_frameworks!
+
+target 'Billables' do
+  pod 'Charts', '~> 2.2'
+end
+
+target 'BillablesTests' do
+
+end
---
+++ Pods/Target Support Files/Pods-BillablesTests/Pods-BillablesTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BillablesTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BillablesTestsVersionString[];
+
